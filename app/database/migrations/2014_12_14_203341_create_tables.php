8f7e16656f499d768f87fa60db19e45b153e3b7f
Max Snyder <maxsnyder@g.harvard.edu>
2014-12-14 10:47:23 -1000

    Add database tables

---
+++ app/database/migrations/2014_12_14_203341_create_tables.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTables extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('users', function($table) 
+		{
+
+		    $table->increments('id');
+		    $table->string('username')->unique();
+		    $table->string('remember_token',100); 
+		    $table->string('password');
+		    $table->timestamps();
+
+		});
+
+		Schema::create('scores', function($table)
+		{
+			$table->increments('id');
+			$table->integer('score');
+		});
+
+		Schema::create('user_score', function($table)
+		{
+			$table->increments('user_id')->unsigned();
+			$table->integer('score_id')->unsigned();
+
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->foreign('score_id')->references('id')->on('scores');
+		});
+
+
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('users');
+		Schema::drop('scores');
+		Schema::drop('user_score');
+	}
+
+}
