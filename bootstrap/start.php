195943a11ea0f1f22300e3e3099ad8d7da515b2e
Max Snyder <maxsnyder@g.harvard.edu>
2014-12-14 11:23:34 -1000

    environment config

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,8 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('*'),
-
+	//'local' => array('Max-HP'),
+	'production' => array('*')
 ));
 
 /*
